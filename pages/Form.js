19edcc45b305b1f0b590225e4057ba5d4347b149
PTBYSR <paul.emechebe@gmail.com>
2022-10-06 05:32:44 +0100

    deploy fix

--- pages/Form.js
+++ pages/Form.js
@@ -31,73 +31,10 @@ const Form = ({ setForm, form, onGenerateId }) => {
   return (
     <>
       <Stack
-        // spacing={{ base: 4, md: 8, lg: 20 }}
-        // direction={{ base: "column", md: "row" }}
       >
         <Stack
-          // align="center"
-          // justify="space-around"
-          // direction={{ base: "row", md: "column" }}
         >
-          {/* <Tooltip
-            label={hasCopied ? "Email Copied!" : "Copy Email"}
-            closeOnClick={false}
-            hasArrow
-          >
-            <IconButton
-              aria-label="email"
-              variant="ghost"
-              size="lg"
-              fontSize="3xl"
-              //   icon={<MdEmail />}
-              _hover={{
-                bg: "blue.500",
-                color: useColorModeValue("white", "gray.700"),
-              }}
-              onClick={onCopy}
-              isRound
-            />
-          </Tooltip> */}
-          {/* <Link href="#">
-                        <IconButton
-                          aria-label="github"
-                          variant="ghost"
-                          size="lg"
-                          fontSize="3xl"
-                          icon={<BsGithub />}
-                          _hover={{
-                            bg: 'blue.500',
-                            color: useColorModeValue('white', 'gray.700'),
-                          }}
-                          isRound
-                        />
-                      </Link>
-                      <Link href="#">
-                        <IconButton
-                          aria-label="twitter"
-                          variant="ghost"
-                          size="lg"
-                          icon={<BsTwitter size="28px" />}
-                          _hover={{
-                            bg: 'blue.500',
-                            color: useColorModeValue('white', 'gray.700'),
-                          }}
-                          isRound
-                        />
-                      </Link>
-                      <Link href="#">
-                        <IconButton
-                          aria-label="linkedin"
-                          variant="ghost"
-                          size="lg"
-                          icon={<BsLinkedin size="28px" />}
-                          _hover={{
-                            bg: 'blue.500',
-                            color: useColorModeValue('white', 'gray.700'),
-                          }}
-                          isRound
-                        />
-                      </Link> */}
+         
         </Stack>
         <Box
           bg={useColorModeValue("white", "gray.700")}
@@ -188,12 +125,6 @@ const Form = ({ setForm, form, onGenerateId }) => {
                 </Select>
               </InputGroup>
             </FormControl>
-            {/* <FormControl isRequired>
-              <FormLabel>Profile picture</FormLabel>
-              <InputGroup>
-                <input type="file" />
-              </InputGroup>
-            </FormControl> */}
             <Button
               colorScheme="blue"
               bg="#66cee8"
